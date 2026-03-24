9a9c2998cf503b6007517f429542b9ef6f5a85f7
lokve007 <149747716+lokve007@users.noreply.github.com>
2026-03-24 11:23:12 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title>Home | American Intercon School</title>
     <meta name="description" content="AIS Main Homepage"/>
-  <!--   <link rel="stylesheet" href="{{ asset('css/home.css') }}"> --}}
+   <link rel="stylesheet" href="{{ asset('css/home.css') }}"> 
     <link rel="stylesheet" href="{{ env('APP_URL') }}/css/home.css">
 </head>
 <body>
